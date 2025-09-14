86785b894bcee9b45e44d3b8bce0ab6d91a0e6f9
Caio Nascimento Battista <nascimento_caio_@hotmail.com>
2025-09-13 23:44:24 -0300

    chore(data): adiciona dados estáticos para o feed FINTA

---
+++ src/data/fintaVideos.js
@@ -0,0 +1,38 @@
+export const fintaVideos = [
+  {
+    id: 1,
+    user: {
+      name: 'Renata Atleta',
+      avatar: 'https://placehold.co/40x40/facc15/1f2937?text=R',
+    },
+    videoUrl:
+      'https://static.videezy.com/system/resources/previews/000/043/279/original/female-soccer-player-dribbling.mp4',
+    caption: 'Trabalhando no controle de bola! ⚽️ #futebolfeminino',
+    likes: 1245,
+    comments: 89,
+  },
+  {
+    id: 2,
+    user: {
+      name: 'Julia Goleira',
+      avatar: 'https://placehold.co/40x40/4ade80/1f2937?text=J',
+    },
+    videoUrl:
+      'https://static.videezy.com/system/resources/previews/000/043/276/original/goal-keeper-on-the-goal-line.mp4',
+    caption: 'Foco total no treino de hoje! 🧤',
+    likes: 2310,
+    comments: 150,
+  },
+  {
+    id: 3,
+    user: {
+      name: 'Bia Zagueira',
+      avatar: 'https://placehold.co/40x40/f87171/1f2937?text=B',
+    },
+    videoUrl:
+      'https://static.videezy.com/system/resources/previews/000/052/934/original/slow-motion-of-a-female-soccer-player-running-and-kicking-the-ball-in-a-stadium-at-night.mp4',
+    caption: 'Aquele chute de longe pra testar a mira! 🎯',
+    likes: 987,
+    comments: 65,
+  },
+];
