dfceed75b6067e8b4613633f5d24ad85099ebdad
Caio Nascimento Battista <nascimento_caio_@hotmail.com>
2025-09-14 15:05:12 -0300

    feat(chatbot): Adiciona componente de chatbot no layout base

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import SideNavBar from './SideNavbar.jsx';
 import BottomNavBar from './BottomNavbar.jsx';
+import Chatbot from './ChatBot.jsx';
 
 //Cria o "elemento pai" de todas as seções, elas serão renderizadas dentro deste layout,
 //que denpendendo do tamanho da tela, renderiza o navbar no inferior ou lateral
@@ -8,6 +9,7 @@ const Layout = () => {
   return (
     <div className="w-full h-screen bg-white dark:bg-gray-900 flex font-sans">
       <SideNavBar />
+      <Chatbot />
       <div className="flex-1 flex flex-col overflow-hidden">
         <main className="flex-1 overflow-y-auto w-full h-full pb-16 md:pb-0">
           {/* O Outlet renderiza o componente da rota filha (HubPage, Finta, etc.) */}
